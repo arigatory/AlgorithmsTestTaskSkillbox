cd063b5c08df621340225ea5ef1c73e3871c1fc1
Ivan Panchenko <arigatory@gmail.com>
2022-06-27 12:48:40 +0300

    Add project files.

---
+++ Solutions/Task1.cs
@@ -0,0 +1,54 @@
+﻿namespace Task1;
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+
+    /*
+    
+    Временная сложность: O(n+m) 
+    Поскольку ровно один из l1 и l2 увеличивается на каждой итерации цикла, 
+    цикл while выполняется в течение количества итераций, равного сумме длин двух списков. 
+    Вся остальная работа ограничивается констанкой, поэтому общая сложность является линейной.
+
+    Пространственная сложность: O(1) 
+    Итеративный подход выделяет только несколько указателей, поэтому он имеет постоянный общий объем памяти.
+    */
+
+    public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+    {
+        // поддерживать неизменную ссылку на узел перед возвращаемым узлом.
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null)
+        {
+            if (l1.val <= l2.val)
+            {
+                prev.next = l1;
+                l1 = l1.next;
+            }
+            else
+            {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // По крайней мере, один из l1 и l2 может все еще иметь узлы в этой точке, так что соединим
+        // ненулевой список в концe объединенного списка.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+}
---
+++ Solutions/Task2.cs
@@ -0,0 +1,58 @@
+﻿namespace Task2;
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution
+{
+    /*
+    Сложность.
+    Пусть N будет количеством узлов в дереве. 
+    
+    Временная сложность: O(N).
+    Это связано с тем, что в рекурсивной функции longestPath мы входим и выходим из каждого узла только один раз. 
+    Мы знаем это, потому что каждый узел идет от своего родителя, 
+    а в дереве у узлов есть только один родитель. 
+    
+    Пространственная сложность: O(N). 
+    Пространственная сложность зависит от размера нашего неявного стека вызовов во время обхода DFS, 
+    который связан с высотой дерева. 
+    В худшем случае дерево скошено, поэтому высота дерева равна O(N). 
+    Если дерево сбалансировано, сложность будет O(logN).
+
+    */
+
+
+    private int diameter;
+    public int DiameterOfBinaryTree(TreeNode root)
+    {
+        diameter = 0;
+        longestPath(root);
+        return diameter;
+    }
+    private int longestPath(TreeNode node)
+    {
+        if (node == null) return 0;
+        // рекурсивно находим самый длинный путь в
+        // и левый дочерний элемент, и правый дочерний элемент
+        int leftPath = longestPath(node.left);
+        int rightPath = longestPath(node.right);
+
+        // обновляем диаметр, если leftPath плюс rightPath больше
+        diameter = Math.Max(diameter, leftPath + rightPath);
+
+        // возвращаем самый длинный между leftPath и rightPath;
+        // добавляем 1 для пути, соединяющего узел и его родителя
+        return Math.Max(leftPath, rightPath) + 1;
+    }
+}
