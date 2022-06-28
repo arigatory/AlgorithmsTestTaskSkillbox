5f07ab3b8415b293ba02b387eec66d941519a3a3
Ivan Panchenko <arigatory@gmail.com>
2022-06-28 19:29:40 +0300

    fix indentation in *.py files

--- Solutions/Task1.py
+++ Solutions/Task1.py
@@ -1,30 +1,30 @@
 ﻿class ListNode(object):
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+    def __init__(self, val=0, next_node=None):
+        self.val = val
+        self.next = next_node
 
-    # Временная сложность: O(n+m) 
-    # Поскольку ровно один из l1 и l2 увеличивается на каждой итерации цикла, 
-    # цикл while выполняется в течение количества итераций, равного сумме длин двух списков. 
-    # Вся остальная работа ограничивается константой, поэтому общая сложность является линейной.
-      
-    # Пространственная сложность: O(1) 
-    # Итеративный подход выделяет только несколько указателей, поэтому он имеет постоянный общий объем памяти.
 
-class Solution:
-    def mergeTwoLists(self, l1, l2):
-        prehead = ListNode(-1)
+# Временная сложность: O(n+m)
+# Поскольку ровно один из l1 и l2 увеличивается на каждой итерации цикла,
+# цикл while выполняется в течение количества итераций, равного сумме длин двух списков.
+# Вся остальная работа ограничивается константой, поэтому общая сложность является линейной.
 
-        prev = prehead
-        while l1 and l2:
-            if l1.val <= l2.val:
-                prev.next = l1
-                l1 = l1.next
-            else:
-                prev.next = l2
-                l2 = l2.next            
-            prev = prev.next
+# Пространственная сложность: O(1)
+# Итеративный подход выделяет только несколько указателей, поэтому он имеет постоянный общий объем памяти.
 
-        prev.next = l1 if l1 is not None else l2
+def merge_two_lists(l1, l2):
+    prehead = ListNode(-1)
 
-        return prehead.next
+    prev = prehead
+    while l1 and l2:
+        if l1.val <= l2.val:
+            prev.next = l1
+            l1 = l1.next
+        else:
+            prev.next = l2
+            l2 = l2.next
+        prev = prev.next
+
+    prev.next = l1 if l1 is not None else l2
+
+    return prehead.next
--- Solutions/Task2.py
+++ Solutions/Task2.py
@@ -1,8 +1,8 @@
-﻿ class TreeNode:
-     def __init__(self, val=0, left=None, right=None):
-         self.val = val
-         self.left = left
-         self.right = right
+﻿class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
     # Сложность.
     # Пусть N будет количеством узлов в дереве. 
@@ -19,21 +19,20 @@
     # Если дерево сбалансировано, сложность будет O(logN).
 
 
-class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        diameter = 0
+def diameter_of_binary_tree(root: TreeNode) -> int:
+    diameter = 0
 
-        def longest_path(node):
-            if not node:
-                return 0
-            nonlocal diameter
+    def longest_path(node):
+        if not node:
+            return 0
+        nonlocal diameter
 
-            left_path = longest_path(node.left)
-            right_path = longest_path(node.right)
+        left_path = longest_path(node.left)
+        right_path = longest_path(node.right)
 
-            diameter = max(diameter, left_path + right_path)
+        diameter = max(diameter, left_path + right_path)
 
-            return max(left_path, right_path) + 1
+        return max(left_path, right_path) + 1
 
-        longest_path(root)
-        return diameter
+    longest_path(root)
+    return diameter
